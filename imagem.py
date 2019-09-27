c3cd10466ea94d6282b213feeb7799d65082fabd
Joas <joasweslei@gmail.com>
2019-09-27 16:30:50 -0300

    Foi adicionada a função de geração do gráfico de desempenho do treinamento. Além disso, foram adicionadas novas imagens para serem usadas no treinamento.

--- imagem.py
+++ imagem.py
@@ -14,7 +14,7 @@ def getDataImage( path):
     pixels = img.load() 
     data = []
     pixel = []
-    for i in range( img.size[0]):        # for every col do: img.size[0]
+    for i in range( img.size[0]):         # for every col do: img.size[0]
         for j in range( img.size[1] ):    # for every row   img.size[1]      
              pixel = pixels[i,j]          # get every pixel
              data.append( pixel[0] )
@@ -26,25 +26,41 @@ def getDataImage( path):
     exif_data
     return data
 
-data1 = getDataImage('6c.png')
-data2 = getDataImage('6d.png')
-size = len( data1 )
-net = buildNetwork( size, 12, 12, 4 )
-ds = SupervisedDataSet( len( data1 ), 4)
-ds.addSample( data1,(6, 6, 6, 6) )
-ds.addSample( data2,(6, 6, 6, 6) )
+dataTraining =  getDataImage( 'img\\7b.png' )
+size = 40 * 40 * 3
 
-trainer = BackpropTrainer(net, ds)
-error = 10
+network = buildNetwork( size, 12, 12, 4 )  # define network
+dataSet = SupervisedDataSet( size, 4 )     # define dataSet
+
+
+# load dataSet
+dataSet.addSample ( getDataImage( 'img\\7b.png' ), (7, 7, 7, 7) )       # 7anos
+dataSet.addSample ( getDataImage( 'img\\7c.png' ), (7, 7, 7, 7) )       # 7anos
+dataSet.addSample ( getDataImage( 'img\\7d.png' ), (7, 7, 7, 7) )       # 7anos
+dataSet.addSample ( getDataImage( 'img\\7e.png' ), (7, 7, 7, 7) )       # 7anos
+dataSet.addSample ( getDataImage( 'img\\18b.png' ), (18, 18, 18, 18) )  # 18 anos
+dataSet.addSample ( getDataImage( 'img\\18c.png' ), (18, 18, 18, 18) )  # 18 anos
+dataSet.addSample ( getDataImage( 'img\\18d.png' ), (18, 18, 18, 18) )  # 18 anos
+dataSet.addSample ( getDataImage( 'img\\18e.png' ), (18, 18, 18, 18) )  # 18 anos
+
+# trainer
+trainer = BackpropTrainer( network, dataSet)
+error = 1
 iteration = 0
+outputs = []
 while error > 0.001: 
     error = trainer.train()
-    iteration += 1
+    outputs.append( error )
+    iteration += 1    
     print ( iteration, error )
 
-print ("\nResult: ", net.activate( getDataImage('6ct.png') ))
-print ("\nResult: ", net.activate( data2 ))
+#name = 'img\\6ct.png'
+#print ("\nResult: ", name, ": ", net.activate( getDataImage(name) ))
+#print ("\nResult: img\\6d.png: ", net.activate( data3 ))
 
-# Result: 2,035,868 0.12753658725156106
-# Result: 2,604,332 0.127530177993413
-
+# plot graph
+plt.ioff()
+plt.plot( outputs )
+plt.xlabel('Iterações')
+plt.ylabel('Erro Quadrático')
+plt.show()
